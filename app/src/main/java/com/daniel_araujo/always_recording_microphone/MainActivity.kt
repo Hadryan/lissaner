dd0f81d8303a99f389dbe3025a9f65ef4da319ce
Daniel Araujo <contact@daniel-araujo.pt>
2020-08-16 11:16:55 +0100

    Generate project files in Android Studio 4.0.1

---
+++ app/src/main/java/com/daniel_araujo/always_recording_microphone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.daniel_araujo.always_recording_microphone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
